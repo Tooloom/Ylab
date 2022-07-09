e9341a939771f78ae4367985cc1ceacfad5d90d8
Tooloom <lrshat333@gmail.com>
2022-07-09 13:25:51 +0300

    homework-2 is completed

---
+++ homework-2/Task_1.py
@@ -0,0 +1,21 @@
+class CyclicIterator:
+    def __init__(self, obj):
+        self.obj = obj
+        self.it = iter(self.obj)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        try:
+            return next(self.it)
+        except StopIteration:
+            self.it = iter(self.obj)
+            return next(self.it)
+
+
+# --------------------- main ---------------------
+if __name__ == '__main__':
+    cyclic_iterator = CyclicIterator(range(3))
+    for i in cyclic_iterator:
+        print(i)
---
+++ homework-2/Task_2.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+from datetime import datetime, timedelta  # added timedelta
+from typing import Generator, List, Tuple
+
+
+@dataclass
+class Movie:
+    title: str
+    dates: List[Tuple[datetime, datetime]]
+
+    def schedule(self) -> Generator[datetime, None, None]:
+        for date in self.dates:
+            day_step = 0
+            while date[0] + timedelta(days=day_step) <= date[1]:
+                yield date[0] + timedelta(days=day_step)
+                day_step += 1
+
+
+# --------------------- main ---------------------
+if __name__ == '__main__':
+    m = Movie('sw', [
+        (datetime(2020, 1, 1), datetime(2020, 1, 7)),
+        (datetime(2020, 1, 15), datetime(2020, 2, 7))
+    ])
+
+    for d in m.schedule():
+        print(d)
